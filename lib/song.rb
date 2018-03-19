d903d3fd3239494712cd7d25503dd6121d6e1227
Andrew Kairys <andrewkairys@gmail.com>
2018-03-19 15:06:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,4 +15,13 @@ class Song
     song.save
     song
   end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|s| s.name == song_name}
+  end
 end
